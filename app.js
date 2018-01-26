4d93328e72c7ccc1f3aacd8fae76141f84ebf2fa
Peter Ashcroft <peter.ashcroft@env.ethz.ch>
2018-01-26 13:24:22 +0100

    Updated to variable-designated colours/. And now things are purple

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 var main = function() {
-    /* Push the body and the nav over by 200px over */
-    $('.icon-menu-open').on('click mouseenter', function() {
-	$('body').animate({left: "200px"}, 200);
-	$('.menu').animate({left: "0px"}, 200).css("z-index","101");
-    });
+  /* Push the body and the nav over by 200px over */
+  $('.icon-menu-open').on('click mouseenter', function() {
+    $('body').animate({
+      left: "200px"
+    }, 200);
+    $('.menu').animate({
+      left: "0px"
+    }, 200).css("z-index", "101");
+  });
 
-    /* Then push them back */
-    $('.menu').on('click mouseleave', function() {
-	$('.menu').animate({left: "-200px"}, 200).css("z-index","99");
-	$('body').animate({left: "0px"}, 200);
-    });
+  /* Then push them back */
+  $('.menu').on('click mouseleave', function() {
+    $('.menu').animate({
+      left: "-200px"
+    }, 200).css("z-index", "99");
+    $('body').animate({
+      left: "0px"
+    }, 200);
+  });
 };
 
 
 $(document).ready(main);
 
 /* Scroll includes buffer to avoid top-menu-bar */
-$(window).on("hashchange", function () {
-    window.scrollTo(window.scrollX, window.scrollY - 53);
+$(window).on("hashchange", function() {
+  window.scrollTo(window.scrollX, window.scrollY - 53);
 });
 
 
 $('.publications .years ol').each(function() {
-    var val=parseFloat($(this).attr("start")) + 1;
-    $(this).css('counter-reset','c '+ val);
+  var val = parseFloat($(this).attr("start")) + 1;
+  $(this).css('counter-reset', 'c ' + val);
 });
