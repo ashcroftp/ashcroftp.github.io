b78aec9a2513d62d467ec81022a0feba399a490e
Peter Ashcroft <peter.ashcroft@env.ethz.ch>
2018-01-26 15:58:23 +0100

    New horizontal menu

--- app.js
+++ app.js
@@ -1,35 +1,10 @@
-var main = function() {
-  /* Push the body and the nav over by 200px over */
-  $('.icon-menu-open').on('click mouseenter', function() {
-    $('body').animate({
-      left: "200px"
-    }, 200);
-    $('.menu').animate({
-      left: "0px"
-    }, 200).css("z-index", "101");
-  });
-
-  /* Then push them back */
-  $('.menu').on('click mouseleave', function() {
-    $('.menu').animate({
-      left: "-200px"
-    }, 200).css("z-index", "99");
-    $('body').animate({
-      left: "0px"
-    }, 200);
-  });
-};
-
-
-$(document).ready(main);
-
-/* Scroll includes buffer to avoid top-menu-bar */
-$(window).on("hashchange", function() {
-  window.scrollTo(window.scrollX, window.scrollY - 53);
-});
-
-
+/* Numbering for publications */
 $('.publications .years ol').each(function() {
   var val = parseFloat($(this).attr("start")) + 1;
   $(this).css('counter-reset', 'c ' + val);
+});
+
+/* Scroll includes buffer to avoid top-menu-bar */
+$(window).on("hashchange", function() {
+  window.scrollTo(window.scrollX, window.scrollY - 130);
 });
