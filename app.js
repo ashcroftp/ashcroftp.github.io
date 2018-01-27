fe37f2e7d8ad21f6de72f61578a4743ed75e05e3
Peter Ashcroft <peter.ashcroft@env.ethz.ch>
2018-01-27 15:46:17 +0100

    Now organised as tabs

--- app.js
+++ app.js
@@ -8,3 +8,19 @@ $('.publications .years ol').each(function() {
 $(window).on("hashchange", function() {
   window.scrollTo(window.scrollX, window.scrollY - 120);
 });
+
+/* Function to control display of contents by tab */
+function openTab(tabName) {
+    // Hide all elements with class="tab" by default */
+    var i, tab;
+    tab = document.getElementsByClassName("tab");
+    for (i = 0; i < tab.length; i++) {
+        tab[i].style.display = "none";
+    }
+
+    // Show the specific tab
+    document.getElementById(tabName).style.display = "block";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
